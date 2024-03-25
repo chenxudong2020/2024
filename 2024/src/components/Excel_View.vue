aaba5ec6ae707e4810345387e19eed86493182d1
chenxudong2020 <872603935@qq.com>
2024-03-25 14:39:16 +0000

    add

---
+++ 2024/src/components/Excel_View.vue
@@ -0,0 +1,34 @@
+<template>
+    <vue-office-excel
+        :src="excel"
+        style="height: 100vh;"
+        @rendered="renderedHandler"
+        @error="errorHandler"
+    />
+</template>
+
+<script>
+//引入VueOfficeExcel组件
+import VueOfficeExcel from '@vue-office/excel'
+//引入相关样式
+import '@vue-office/excel/lib/index.css'
+
+export default {
+    components: {
+        VueOfficeExcel
+    },
+    data() {
+        return {
+            excel: 'http://static.shanhuxueyuan.com/demo/excel.xlsx'//设置文档地址
+        }
+    },
+    methods: {
+        renderedHandler() {
+            console.log("渲染完成")
+        },
+        errorHandler() {
+            console.log("渲染失败")
+        }
+    }
+}
+</script>
