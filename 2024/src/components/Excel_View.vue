5233849f43eff6805d1f7c81391ec03df19aeaea
chenxudong2020 <872603935@qq.com>
2024-03-25 15:05:03 +0000

    fix

--- 2024/src/components/Excel_View.vue
+++ 2024/src/components/Excel_View.vue
@@ -1,7 +1,7 @@
 <template>
     <vue-office-excel
         :src="excel"
-        style="height: 100vh;"
+        style="height: 90vh;"
         @rendered="renderedHandler"
         @error="errorHandler"
     />
@@ -19,15 +19,24 @@ export default {
     },
     data() {
         return {
-            excel: 'https://github.com/chenxudong2020/2024/raw/main/%E6%AD%A6%E6%B1%89%E5%85%AC%E5%8F%B82024%E5%90%8D%E5%8D%95.xlsx'//设置文档地址
+            options:{
+                xls: false,       //预览xlsx文件设为false；预览xls文件设为true
+                minColLength: 0,  // excel最少渲染多少列，如果想实现xlsx文件内容有几列，就渲染几列，可以将此值设置为0.
+                minRowLength: 0,  // excel最少渲染多少行，如果想实现根据xlsx实际函数渲染，可以将此值设置为0.
+                widthOffset: 10,  //如果渲染出来的结果感觉单元格宽度不够，可以在默认渲染的列表宽度上再加 Npx宽
+                heightOffset: 0, //在默认渲染的列表高度上再加 Npx高
+                beforeTransformData: (workbookData) => {return workbookData}, //底层通过exceljs获取excel文件内容，通过该钩子函数，可以对获取的excel文件内容进行修改，比如某个单元格的数据显示不正确，可以在此自行修改每个单元格的value值。
+                transformData: (workbookData) => {return workbookData}, //将获取到的excel数据进行处理之后且渲染到页面之前，可通过transformData对即将渲染的数据及样式进行修改，此时每个单元格的text值就是即将渲染到页面上的内容
+            },
+            excel: '%E6%AD%A6%E6%B1%89%E5%85%AC%E5%8F%B82024%E5%90%8D%E5%8D%95.xlsx'//设置文档地址
         }
     },
     methods: {
         renderedHandler() {
-            console.log("渲染完成")
+            
         },
         errorHandler() {
-            console.log("渲染失败")
+            
         }
     }
 }
--- 2024/vue.config.js
+++ 2024/vue.config.js
@@ -1,4 +1,17 @@
 const { defineConfig } = require('@vue/cli-service')
 module.exports = defineConfig({
-  transpileDependencies: true
+  transpileDependencies: true,
+  devServer: {
+    proxy: {
+      '/api': {
+        target: 'https://github.com/chenxudong2020/2024/raw/main', // 目标地址
+        changeOrigin: true, // 开启代理，在本地创建一个虚拟服务器，然后发送请求的数据，会同时会收到请求的数据，这样服务端和服务端进行数据的交互就不会有跨域问题
+        pathRewrite: {
+          '^/api': '' // 路径重写，移除路径中的/api
+        }
+      }
+    }
+  }
 })
+
+ 
